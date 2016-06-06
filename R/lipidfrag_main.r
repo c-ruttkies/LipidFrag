619b3002635984a12b1f896c676e877bffc202aa
Christoph Ruttkies <chrisr@troll>
2016-06-06 22:10:33 +0200

    changed path for training data

--- R/lipidfrag_main.r
+++ R/lipidfrag_main.r
@@ -1,5 +1,5 @@
 # generate models
 
-models.pos <- generate.model("model_scores_pos.txt")
-models.neg <- generate.model("model_scores_neg.txt")
+models.pos <- generate.model("../data/model_scores_pos.txt")
+models.neg <- generate.model("../data/model_scores_neg.txt")
 

8ca5a727391427df20962d18e312610a22e59a75
c-ruttkies <cruttkie@ipb-halle.de>
2017-09-12 09:26:30 +0200

    Update lipidfrag_main.r

--- R/lipidfrag_main.r
+++ R/lipidfrag_main.r
@@ -2,9 +2,10 @@
 source("lipidfrag_train.r")
 source("lipidfrag_predict.r")
 
+# initialise the prediction models
 models.pos <- generate.model("../data/model_scores_pos.txt")
 models.neg <- generate.model("../data/model_scores_neg.txt")
 
+# predict the lipid (main-/sub-)class for a given MetFrag annotation
 predicted.lipidmaps.class.neg <- predict.lipidmaps.class("../data/cel_results/neg/Cel_1-A,8_05_21044.914.914.0_variant_0.csv", models.neg, sep="|")
-
 predicted.lipidmaps.class.pos <- predict.lipidmaps.class("../data/cel_results/pos/Cel_AutoMS_2-A,2_05_18615.944.944.0_variant_2.csv", models.pos, sep="|")
